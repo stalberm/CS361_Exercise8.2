2fafc6fd426d7b3987f21be7b686742e40571dcd
Micah Stalberg <stalberm@oregonstate.edu>
2022-11-17 16:11:23 -0800

    Adds Poker Player class integration

--- exercise-8.2.rb
+++ exercise-8.2.rb
@@ -1,13 +1,60 @@
-class Poker
+class Player
+  attr_reader :name
+
+  def initialize(name)
+      @name = name
+  end
+end
+
+class PokerPlayer < Player
+  attr_reader :hand
+
+  def initialize(name, hand)
+      super(name)
+      @hand = hand
+  end
+end
+
+class ChessPlayer < Player
+  def initialize(name, color)
+    super(name)
+    @color = color
+  end
+end
+
+class GoPlayer < Player
+  attr_reader :name, :color
+  
+  def initialize(name, color)
+    super(name)
+    @color = color
+  end
+end
+
+class Game
+  attr_reader :players
+  
   def initialize(players)
     @players = players
-    @hands = []
-    players.length().times { |x| @hands.append(nil) }
+  end
+
+  def get_results()
+    "[pretend these are #{self.class.name} results]"
+  end
+end
+
+
+class Poker < Game
+  def initialize(players)
+    super(players)
+    # @hands = []
+    # players.length().times { |x| @hands.append(nil) }
   end
 
   def play_poker()
     puts "Players in the poker game:"
-    @players.length().times { |i| puts "#{self.get_player_name(i)}: #{self.get_player_hand(i)}" }
+    # @players.length().times { |i| puts "#{self.get_player_name(i)}: #{self.get_player_hand(i)}" }
+    @players.each { |player| puts "#{player.name}: #{player.hand}" }
     # [pretend there's code here]
   end
 
@@ -20,11 +67,32 @@ class Poker
   end
 
   def get_player_hand(i)
-    return @hands[i]
+    # return @hands[i]
+    return @players[i]
   end
 end
 
-class Chess
+class Go < Game
+  # def initialize(players)
+  #   @players = []
+  #   players.each { |x, y| @players.append(GoPlayer.new(x, y)) }
+  # end
+  def initialize(players)
+    super(players)
+  end
+
+  def play()
+    puts "Players in the go game:"
+    @players.each { |player| puts "#{player.name}: #{player.color}" }
+    # [pretend there's code here]
+  end
+
+  def get_score()
+    return "[pretend these are go results]"
+  end
+end
+
+class Chess < Game
   def initialize(players)
     @players = players
   end
@@ -44,31 +112,6 @@ class Chess
   end
 end
 
-class GoPlayer
-  attr_reader :name, :color
-
-  def initialize(name, color)
-    @name = name
-    @color = color
-  end
-end
-
-class Go
-  def initialize(players)
-    @players = []
-    players.each { |x, y| @players.append(GoPlayer.new(x, y)) }
-  end
-
-  def play()
-    puts "Players in the go game:"
-    @players.each { |player| puts "#{player.name}: #{player.color}" }
-    # [pretend there's code here]
-  end
-
-  def get_score()
-    return "[pretend these are go results]"
-  end
-end
 
 class PlayGames
 
@@ -78,17 +121,21 @@ class PlayGames
   end
 
   def play()
+    players = []
     case @game_number
     when 1
-      poker = Poker.new(@player_list)
+      @player_list.each { |x| players.append(PokerPlayer.new(x, nil)) }
+      poker = Poker.new(players)
       poker.play_poker()
       puts poker.get_results()
     when 2
+      
       chess = Chess.new(@player_list)
       chess.play_game()
       puts chess.get_results()
     when 3
-      go = Go.new(@player_list)
+      @player_list.each { |x, y| players.append(GoPlayer.new(x, y)) }
+      go = Go.new(players)
       go.play()
       puts go.get_score()
     end
